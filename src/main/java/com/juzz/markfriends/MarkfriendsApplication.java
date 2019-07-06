c6126c5d3b95e02d1830b8c69255735eb9bd3366
javaly18 <javaly18@163.com>
2019-07-07 02:22:45 +0800

    初始化提交

---
+++ src/main/java/com/juzz/markfriends/MarkfriendsApplication.java
@@ -0,0 +1,13 @@
+package com.juzz.markfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkfriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkfriendsApplication.class, args);
+	}
+
+}
